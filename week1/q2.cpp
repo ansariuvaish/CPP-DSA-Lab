ed4894e77eda41a74787339bafb122267e7db19c
Uvaish Ansari <ansariuvaish123@gmail.com>
2026-09-09 00:49:27 +0530

    Add Week 1 Q2

---
+++ week1/q2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << "Multiplication Table of " << n << ":\n";
+
+    for (int i = 1; i <= 10; i++)
+        cout << n << " x " << i << " = " << n * i << "\n";
+
+    return 0;
+}
